8e38f73a5053f90cf73bf154f29e69634760cf43
Pear104 <schoolpear14@gmail.com>
2023-12-26 10:01:11 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173'], // Replace with your production frontend's domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
